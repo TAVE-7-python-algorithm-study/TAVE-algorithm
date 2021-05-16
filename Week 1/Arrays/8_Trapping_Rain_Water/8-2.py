fbb2746de6b779323f004efdf0fa0a10bb348666
JUNG EUNJIN <65335952+jayjinnie@users.noreply.github.com>
2021-05-16 11:19:54 +0900

    Create 8-2.py

---
+++ Week 1/Arrays/8_Trapping_Rain_Water/8-2.py
@@ -0,0 +1,28 @@
+# 7장 배열 - 8번 빗물 트래핑 (난이도 3)
+from typing import List
+
+# 풀이2. 스택 쌓기
+def trap(height: List[int]) -> int:
+    stack = []
+    volume = 0
+
+    # 막대 기둥 높이만큼 스택에 쌓아 나가다가, 
+    # 막대 기둥 간에 높이 차가 발생하면, 스택에 저장해둔 높이를 하나씩 꺼내서
+    # 이전 막대 기둥과의 높이 차이만큼 volume에 더해주기
+    for i in range(len(height)):
+        while stack and height[i] > height[stack[-1]]: # 변곡점을 만나는 경우
+            top = stack.pop()                          # 스택에서 저장해둔 높이를 꺼낸다
+
+            if not len(stack):
+                break
+
+        distance = i - stack[-1] - 1                  # 이전 막대 기둥과의 차이만큼 물 높이 처리하기
+        waters = min(height[i], height[stack[-1]]) - height[top]
+        volume += distance * waters
+    stack.append(i)
+    return volume
+
+
+def main():
+    ls = [0,1,0,2,1,0,1,3,2,1,2,1]
+    print(trap(ls))
