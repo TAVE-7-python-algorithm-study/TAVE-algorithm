128bc2b75fd3f3ede568f206a6a6b5c247ea9acd
JUNG EUNJIN <65335952+jayjinnie@users.noreply.github.com>
2021-05-26 10:23:24 +0900

    Create 29-1.py

---
+++ LinearDataStructure/HashTables/29_Jewels_and_Stones/29-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        freqs = {}
+        count = 0
+
+        # S의 빈도수 계산
+        for char in S:
+            if char not in freqs:
+                freqs[char] = 1
+            else:
+                frequs[char] += 1
+
+        # J의 빈도수 합산
+        for char in J:
+            if char in freqs:
+                count += freqs[char]
+        return count
