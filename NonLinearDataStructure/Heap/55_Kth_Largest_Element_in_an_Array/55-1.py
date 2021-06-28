147c075a74662af4da24e5cd1c73519f00151983
JUNG EUNJIN <65335952+jayjinnie@users.noreply.github.com>
2021-06-28 16:44:44 +0900

    Update 55-1.py

--- NonLinearDataStructure/Heap/55_Kth_Largest_Element_in_an_Array/55-1.py
+++ NonLinearDataStructure/Heap/55_Kth_Largest_Element_in_an_Array/55-1.py
@@ -1,1 +1,15 @@
+import heapq
+from typing import List
+# 55번 배열의 K번째 큰 요소
 
+# 풀이1. heapq 모듈 이용
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = list()
+        for n in nums:
+            heapq.heappush(heap, -n)
+
+        for _ in range(1, k):
+            heapq.heappop(heap)
+
+        return -heapq.heappop(heap)
