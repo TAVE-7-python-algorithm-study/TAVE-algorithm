1c91f8e189e25403c39ec794c8a9c85b55b03ff1
JUNG EUNJIN <65335952+jayjinnie@users.noreply.github.com>
2021-06-28 15:32:16 +0900

    Create 52-4.py

---
+++ NonLinearDataStructure/Tree/52_Range_Sum_of_BST/52-4.py
@@ -0,0 +1,25 @@
+# 52번 이진 탐색 트리(BST) 합의 범위
+
+# 이진 트리 노드를 정의
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# 풀이 4. 반복 구조 BFS로 필요한 노드 탐색
+class Solution:
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        stack, sum = [root], 0
+        # 큐 연산을 이용해 반복 구조 BFS로 필요한 노드 탐색
+        위에 설명과 마찬가지
+        while stack:
+            node = stack.pop(0)
+            if node:
+                if node.val > L:
+                    stack.append(node.left)
+                if node.val < R:
+                    stack.append(node.right)
+                if L <= node.val <= R:
+                    sum += node.val
+        return sum
