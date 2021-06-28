382b4036232f390055a447dcb70cc6e7188f15ed
JUNG EUNJIN <65335952+jayjinnie@users.noreply.github.com>
2021-06-28 13:48:02 +0900

    Create 45-2.py

---
+++ NonLinearDataStructure/Tree/45_Invert_Binary_Tree/45-2.py
@@ -0,0 +1,25 @@
+import collections
+# 45번 이진 트리 반전
+
+# 풀이 2: 반복 구조로 BFS
+
+# 이진 트리 노드를 정의
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        
+# BFS 풀이를 위해 queue를 정의
+def invertTree(self, root: TreeNode) -> TreeNode:
+    queue = collections.deque([root])
+    
+    while queue:
+        node = queue.popleft()
+        # 부모 노드부터 시작해 내려가며 swap 진행
+        node.left, node.right = node.right, node.left
+        
+        queue.append(node.left)
+        queue.append(node.right)
+        
+    return root
