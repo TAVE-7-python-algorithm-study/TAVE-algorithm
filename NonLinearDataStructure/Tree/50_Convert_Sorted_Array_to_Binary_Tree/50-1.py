bf90b9b18f118bee0534ddf6538797ab1fe652b0
JUNG EUNJIN <65335952+jayjinnie@users.noreply.github.com>
2021-06-28 15:24:36 +0900

    Create 50-1.py

---
+++ NonLinearDataStructure/Tree/50_Convert_Sorted_Array_to_Binary_Tree/50-1.py
@@ -0,0 +1,24 @@
+from typing import List
+
+# 이진 트리 노드를 정의
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# 풀이 1: 이진 검색 결과로 트리 구성
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if not nums:
+            return None
+
+        mid = len(nums) // 2
+
+        # 분할 정복으로 이진 검색 결과 트리 구성
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid + 1:])
+
+        return node
+      
